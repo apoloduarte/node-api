d873af5775a9f916539e0903c73caaba7b0a56ab
apoloduarte <apololeonard@gmail.com>
2023-06-26 21:57:13 -0300

    terceiro commit

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,6 +4,12 @@ class UserController {
     async store(request, response) {
         const {id, name, email, provider} = await User.create(request.body)
 
+        // const userExist = await User.findOne({where: {email: request.body.email}})
+
+        if (userExist) {
+            return response.status(400).json({erro: "email ja cadastrado"})
+        }        
+
         return response.json({
             id,
             name,
--- src/database/index.js
+++ src/database/index.js
@@ -1,20 +1,19 @@
-import { Sequelize }  from 'sequelize'
+import  { Sequelize } from 'sequelize'
 
 import User from '../app/models/User'
 
-import { databaseConfig } from '../config/database'
+import  databaseConfig  from '../config/database'
 
-const models = {User}
+const models = [User]
 
 class Database {
-    constructor(){
+    constructor() {
         this.init()
     }
     init() {
-        this.connection = new Sequelize(databaseConfig)
-
-        models.map( model => model.init(this.connection))
-            .map(model => model.associate && model.associate(this.connection.models))
+		this.connection = new Sequelize(databaseConfig);
+		
+		models.map( model => model.init(this.connection))
     }
 }
 
